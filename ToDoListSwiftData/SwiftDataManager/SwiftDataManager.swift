8afe8e3710be39617964e0a09e23e5a1a73c091c
Carlos Barrón <vn51vl1@wal-mart.com>
2024-08-31 14:42:55 -0600

    Feat: add manager for to do's

---
+++ ToDoListSwiftData/SwiftDataManager/SwiftDataManager.swift
@@ -0,0 +1,59 @@
+//
+//  SwiftDataManager.swift
+//  ToDoListSwiftData
+//
+//  Created by Carlos Barron on 31/08/24.
+//
+
+import Foundation
+import SwiftData
+
+protocol ToDoManager {
+    func addNewData(title: String, description: String)
+    func updateData(id: UUID, title: String, description: String, isFinished: Bool)
+    func fetchData() -> [ToDo]
+    func deleteData(toDo: ToDo)
+}
+
+class SwiftDataManager: ToDoManager {
+
+    private var container: ModelContainer?
+
+    init() {
+        do {
+            container = try ModelContainer(for: ToDo.self)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    @MainActor 
+    func addNewData(title: String, description: String) {
+        container?.mainContext.insert(ToDo(id: UUID(), title: title, toDoDescription: description, isFinished: false))
+    }
+
+    @MainActor
+    func updateData(id: UUID, title: String, description: String, isFinished: Bool) {
+        container?.mainContext.insert(ToDo(id: id, title: title, toDoDescription: description, isFinished: isFinished))
+    }
+
+    @MainActor 
+    func fetchData() -> [ToDo] {
+        do {
+            let descriptor = FetchDescriptor<ToDo>()
+            guard let items = try container?.mainContext.fetch(descriptor) else {
+                return []
+            }
+
+            return items
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
+    }
+
+    @MainActor
+    func deleteData(toDo: ToDo) {
+        container?.mainContext.delete(toDo)
+    }
+}
